b2d88e4bb88ed83795ca3ca8492d3c5477fbc569
Azikenp <azikenp26@gmail.com>
2023-08-06 16:51:59 +0100

    adding spinner and error message

--- src/script.js
+++ src/script.js
@@ -2,16 +2,21 @@ const searchBtn = document.querySelector('.search');
 const result = document.querySelector('.results');
 
 const renderMeaning = function(data){
+    const word = data.word;
+    const partOfSpeech = data.meanings[0].partOfSpeech;
+    const phonetic = data.phonetic? data.phonetic: ""
+    const meaning = data.meanings[0].definitions[0].definition;
+
     const html = `
             <div class="results--header">
-                <h3 class="text-md font-bold text-gray-700">${data.word}</h3>
+                <h3 class="text-md font-bold text-gray-700">${word}</h3>
             </div>
             <div class="part--of--speech text-sm text-gray-400 flex mb-8">
-                <p class="mr-2">${data.meanings[0].partOfSpeech}</p>
-                <span>${data.phonetic}</span>
+                <p class="mr-2">${partOfSpeech}</p>
+                <span>${phonetic}</span>
             </div>
             <div class="results--definition text-gray-600 mb-4">
-                <p>${data.meanings[0].definitions[0].definition}</p>
+                <p>${meaning}</p>
             </div>
             <div class="results--sentence border-l-4 pl-2 pr-12">
                 <p>her eyes were closed and she looked very serene </p>
@@ -29,21 +34,51 @@ const renderSpinner = function(){
         result.insertAdjacentHTML('beforeend', html);
 }
 
+const renderErrorOne = function (){
+    const html = `
+            <div class="error  text-center py-8">
+                <p class="text-gray-400">Ooops!!! <br> We could't find that word, please try searching for another word.</p>
+            </div>
+        `;
+        result.insertAdjacentHTML('beforeend', html);
+}
+
+const renderErrorTwo = function (){
+    const html = `
+            <div class="error  text-center py-8">
+                <p class="text-gray-400">Please enter a word!</p>
+            </div>
+        `;
+        result.insertAdjacentHTML('beforeend', html);
+}
+
 // renderSpinner()
 
 const api = fetch('https://api.dictionaryapi.dev/api/v2/entries/en/cursed').then(res => res.json()).then(data => console.log(data))
 
 const getMeaning = function(word){
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    .then(res => res.json())
-    .then(data => renderMeaning(data[0]))
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(res => res.json())
+        .then(data => renderMeaning(data[0]))
+        .catch(error => {
+            word.length > 0 ? renderErrorOne()  : renderErrorTwo();
+        });
 }
+
+const clearView = function(){
+    result.textContent = ""
+}
+
 
 
 searchBtn.addEventListener('click', function(e){
     e.preventDefault();
-    let searchInput = document.querySelector('input');
-    result.textContent = ""
-    getMeaning(searchInput.value);
-    searchInput.value = ""
+    clearView();
+    renderSpinner();
+    setTimeout(() => {
+        let searchInput = document.querySelector('input');
+        clearView();
+        getMeaning(searchInput.value);
+        searchInput.value = ""
+    }, 500);
 })
