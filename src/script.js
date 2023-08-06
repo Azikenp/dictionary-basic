60054de73a203059b44e19f80035b673f5433d76
Azikenp <azikenp26@gmail.com>
2023-08-06 15:06:13 +0100

    rendering meaning

--- src/script.js
+++ src/script.js
@@ -1,3 +1,35 @@
 const searchBtn = document.querySelector('.search');
+const result = document.querySelector('.results');
 
-const api = fetch('https://api.dictionaryapi.dev/api/v2/entries/en/data').then(res => res.json()).then(data => console.log(data[0].meanings))
+const renderMeaning = function(data){
+    const html = `
+            <div class="results--header">
+                <h3 class="text-md font-bold text-gray-700">${data.word}</h3>
+            </div>
+            <div class="part--of--speech text-sm text-gray-400 flex mb-8">
+                <p class="mr-2">${data.meanings[0].partOfSpeech}</p>
+                <span>${data.phonetic}</span>
+            </div>
+            <div class="results--definition text-gray-600 mb-4">
+                <p>${data.meanings[0].definitions[0].definition}</p>
+            </div>
+            <div class="results--sentence border-l-4 pl-2 pr-12">
+                <p>her eyes were closed and she looked very serene </p>
+            </div>
+        `;
+        result.insertAdjacentHTML('beforeend', html);
+}
+
+const api = fetch('https://api.dictionaryapi.dev/api/v2/entries/en/cursed').then(res => res.json()).then(data => console.log(data))
+
+const getMeaning = function(word){
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(res => res.json())
+    .then(data => renderMeaning(data[0]))
+}
+getMeaning("head")
+
+searchBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log("ehllo");
+})
